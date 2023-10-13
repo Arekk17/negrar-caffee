0d8a103897d2094382a9100286f3463e3cc0163e
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-10-13 13:37:49 +0200

    modify navigation

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,43 +1,41 @@
 'use client'
-import React from "react";
-import Image from 'next/image'
-import logo from '@/assets/NEGRAR.png'
+import React, { useEffect, useState } from "react";
+import Image from 'next/image';
+import logo from '@/assets/NEGRAR.png';
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
+
 import styles from './Navigation.module.css';
-import { useSearchParams } from 'next/navigation'
 
 export const Navigation = () => {
-  const searchParams = useSearchParams()
-  let backgroundImage = '';
-  let overlayClass = '';
+  const pathname = usePathname();
+  console.log(pathname)
+  const [isHome, setIsHome] = useState(false);
 
-  if (searchParams.get('/') === null) {
-    backgroundImage = 'transparent';
-    overlayClass = styles.transparentOverlay;
-  } else {
-    backgroundImage = '../../../assets/bgnav.png';
-    overlayClass = styles.darkOverlay;
-  }
-  
-  return(
-    <nav className={`${styles.navigation} ${overlayClass}`} style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <div className="logo">
-            <Image
-                src={logo}
-                width={146}
-                height={88}
-                alt="logo"
-            />
-            </div>
-            <div className={styles.options}>
-                <Link href='/' className={styles.option}>Home</Link>
-                <Link href='/about' className={styles.option}>O nas</Link>
-                <Link href='/menu' className={styles.option}>Menu</Link>
-                <Link href='/product' className={styles.option}>Produkty</Link>
-                <Link href='/localization' className={styles.option}>Lokalizacja</Link>
-            </div>
+  useEffect(() => {
+    setIsHome(pathname === '/');
+  }, [pathname]);
+
+  const backgroundImage = isHome ? 'none' : `url('../../../assets/bgnav.png')`;
+  const overlayClass = isHome ? 'transparent-background' : '';
+
+  return (
+    <nav className={`${styles.navigation} ${overlayClass}`} style={{ backgroundImage }}>
+      <div className="logo">
+        <Image
+          src={logo}
+          width={146}
+          height={88}
+          alt="logo"
+        />
+      </div>
+      <div className={styles.options}>
+        <Link href='/' className={styles.option}>Home</Link>
+        <Link href='/about' className={styles.option}>O nas</Link>
+        <Link href='/menu' className={styles.option}>Menu</Link>
+        <Link href='/product' className={styles.option}>Produkty</Link>
+        <Link href='/localization' className={styles.option}>Lokalizacja</Link>
+      </div>
     </nav>
-  )
+  );
 };
-
-
