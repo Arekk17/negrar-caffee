644b7bebf7ab1727610f1ef0fafb1b02bcb3cc6b
Arek Daszkowski <daszkowskiarek145@gmail.com>
2024-01-29 12:43:55 +0100

    add responsive to slider and time open

--- src/app/components/Sliders/Slider.tsx
+++ src/app/components/Sliders/Slider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { menuItem } from '../OurMenu/menuData'
 import { LeftArrow } from '@/assets/Icon/LeftArrow'
 import { RightArrow } from '@/assets/Icon/RightArrow'
@@ -7,10 +7,20 @@ import { ProductCard } from '../Card/ProductCard'
 
 export const CaffeeSlider = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null)
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   const scroll = (direction: 'left' | 'right') => {
     if (scrollContainerRef.current) {
-      const scrollAmount = 1200
+      const scrollAmount = isMobile ? 300 : 1200
       const scrollDirection = direction === 'left' ? -scrollAmount : scrollAmount
 
       scrollContainerRef.current.scrollBy({ left: scrollDirection, behavior: 'smooth' })
@@ -19,24 +29,27 @@ export const CaffeeSlider = () => {
 
   return (
     <>
-      <div className='relative flex items-center mt-[43px] ml-[10px] w-[1155px]'>
-        <button
-          onClick={() => scroll('left')}
-          className='absolute left-[-45px] z-10 bg-white bg-opacity-80 w-[50px] h-[50px]  flex items-center justify-center rounded-2xl'
-        >
-          <LeftArrow />
-        </button>
-        <button
-          onClick={() => scroll('right')}
-          className='absolute right-[-45px] z-10 bg-white bg-opacity-80 w-[50px] h-[50px] hover:bg-white flex items-center justify-center rounded-2xl'
-        >
-          <RightArrow />
-        </button>
+      <div className='relative flex items-center mt-[43px] ml-[10px] w-full md:w-[1200px]'>
+        {!isMobile && (
+          <button
+            onClick={() => scroll('left')}
+            className='absolute left-[-45px]  z-10 bg-white bg-opacity-80 w-[50px] h-[50px] flex items-center justify-center rounded-2xl'
+          >
+            <LeftArrow />
+          </button>
+        )}
+        {!isMobile && (
+          <button
+            onClick={() => scroll('right')}
+            className='absolute right-[-5px]  z-10 bg-white bg-opacity-80 w-[50px] h-[50px] flex items-center justify-center rounded-2xl'
+          >
+            <RightArrow />
+          </button>
+        )}
 
         <div
           ref={scrollContainerRef}
-          style={{ display: 'flex', overflow: 'hidden' }}
-          className='scroll-container'
+          className={`scroll-container flex ${isMobile ? 'overflow-x-auto' : 'md:overflow-hidden'}`}
         >
           {menuItem.map((item) => (
             <ProductCard
--- src/app/components/TimeOpen/TimeOpen.tsx
+++ src/app/components/TimeOpen/TimeOpen.tsx
@@ -1,31 +1,27 @@
-import React from 'react'
-
-const TimeOpen = () => {
-  return (
-    <div className='timeOpenBackground h-auto md:h-[440px] flex flex-col md:flex-row items-center justify-center md:justify-start gap-4 md:gap-[100px] p-4 md:p-0'>
-      <div className='text-white text-s lg:text-l bg-brownTransparent w-full md:w-[540px] h-[137px] flex flex-col items-center justify-center md:ml-[100px] mb-4 md:mb-0'>
-        <h2 className='font-bold mb-2'>GODZINY OTWARCIA</h2>
+const TimeOpen = () => (
+  <div className='timeOpenBackground h-auto md:h-[440px] flex flex-col md:flex-row items-center justify-center md:justify-start gap-4 md:gap-[100px] p-4 md:p-0'>
+    <div className='text-white text-s lg:text-l bg-brownTransparent w-full md:w-[540px] h-[137px] flex flex-col items-center justify-center md:ml-[100px] mb-4 md:mb-0'>
+      <h2 className='font-bold mb-2'>GODZINY OTWARCIA</h2>
+    </div>
+    <div className='grid grid-cols-2 gap-4 text-white text-s lg:text-m '>
+      <div className='flex flex-col items-start justify-center'>
+        <span>Mon-Thu</span>
+        <span>08:00-18:00</span>
       </div>
-      <div className='grid grid-cols-2 gap-4 text-white text-s lg:text-m '>
-        <div className='flex flex-col items-start justify-center'>
-          <span>Mon-Thu</span>
-          <span>08:00-18:00</span>
-        </div>
-        <div className='flex flex-col items-start justify-center'>
-          <span>Fri</span>
-          <span>08:00-19:00</span>
-        </div>
-        <div className='flex flex-col items-start justify-center'>
-          <span>Sat</span>
-          <span>9:00-18:00</span>
-        </div>
-        <div className='flex flex-col items-start justify-center'>
-          <span>Sun</span>
-          <span>9:00-18:00</span>
-        </div>
+      <div className='flex flex-col items-start justify-center'>
+        <span>Fri</span>
+        <span>08:00-19:00</span>
+      </div>
+      <div className='flex flex-col items-start justify-center'>
+        <span>Sat</span>
+        <span>9:00-18:00</span>
+      </div>
+      <div className='flex flex-col items-start justify-center'>
+        <span>Sun</span>
+        <span>9:00-18:00</span>
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 export default TimeOpen
