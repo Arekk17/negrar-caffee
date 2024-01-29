886da613bc15412e4333cf77ae6656c9a45d5476
Arek Daszkowski <daszkowskiarek145@gmail.com>
2024-01-29 13:19:31 +0100

    add dividedsection responsive

--- src/app/components/layout/DividedSections/DividedSections.tsx
+++ src/app/components/layout/DividedSections/DividedSections.tsx
@@ -1,16 +1,16 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 
 interface SignInLayoutProps {
-  LeftSection: ReactNode
-  RightSection: ReactNode
+  LeftSection: React.ReactNode
+  RightSection: React.ReactNode
 }
 
 export const DividedSection: React.FC<SignInLayoutProps> = ({ LeftSection, RightSection }) => {
   return (
-    <div className='w-full h-screen flex items-center justify-center signinBackground'>
-      {LeftSection}
-      <div className='h-[488px] border-l border-solid border-brown ml-10' />
-      <div className='max-w-[500px] flex items-center justify-center flex-col text-center space-y-4'>{RightSection}</div>
+    <div className='flex flex-col md:flex-row w-full min-h-screen items-center justify-center signinBackground'>
+      <div className='md:w-1/4 w-3/4 flex justify-center items-center'>{LeftSection}</div>
+      <div className='hidden md:block h-[488px] border-l border-solid border-brown ml-10' />
+      <div className='md:w-1/4 w-full max-w-md flex items-center justify-center flex-col text-center space-y-4 p-4'>{RightSection}</div>
     </div>
   )
 }
