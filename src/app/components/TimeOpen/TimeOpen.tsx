5e7cd257795a92b468b0cf6eed235b0f36763007
Arek Daszkowski <daszkowskiarek145@gmail.com>
2024-01-29 12:04:18 +0100

    add responsive to timeOpen

--- src/app/components/TimeOpen/TimeOpen.tsx
+++ src/app/components/TimeOpen/TimeOpen.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const TimeOpen = () => {
   return (
-    <div className='timeOpenBackground h-[440px] flex items-center justify-start gap-[100px]'>
-      <div className='text-white text-m bg-brownTransparent w-[540px] h-[137px] flex flex-col items-center justify-center ml-[100px] p-4'>
-        <h2 className='font-bold text-m mb-2'>GODZINY OTWARCIA</h2>
+    <div className='timeOpenBackground h-auto md:h-[440px] flex flex-col md:flex-row items-center justify-center md:justify-start gap-4 md:gap-[100px] p-4 md:p-0'>
+      <div className='text-white text-s lg:text-l bg-brownTransparent w-full md:w-[540px] h-[137px] flex flex-col items-center justify-center md:ml-[100px] mb-4 md:mb-0'>
+        <h2 className='font-bold mb-2'>GODZINY OTWARCIA</h2>
       </div>
-      <div className='grid grid-cols-2 gap-4 text-white text-m'>
+      <div className='grid grid-cols-2 gap-4 text-white text-s lg:text-m '>
         <div className='flex flex-col items-start justify-center'>
           <span>Mon-Thu</span>
           <span>08:00-18:00</span>
