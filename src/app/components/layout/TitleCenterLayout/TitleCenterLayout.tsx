a5529650347478f833b35ead53ab5b2d3e478a33
Arek Daszkowski <daszkowskiarek145@gmail.com>
2024-02-08 14:43:44 +0100

    change height

--- src/app/components/layout/TitleCenterLayout/TitleCenterLayout.tsx
+++ src/app/components/layout/TitleCenterLayout/TitleCenterLayout.tsx
@@ -5,7 +5,7 @@ type TTitleCenterLayout = {
   children: ReactNode
 }
 export const TitleCenterLayout = ({ background, title, children }: TTitleCenterLayout) => (
-  <div className={`w-full h-screen ${background}`}>
+  <div className={`w-full h-full ${background}`}>
     <div className='flex w-full items-center justify-center pt-10'>
       <h2 className='text-l text-brownDark font-semibold'>{title}</h2>
     </div>
